ef06bb16f8ae3d040e7b740f46a53068478f710f
Louise <louiseti@kth.se>
2023-02-27 19:19:16 +0100

    Doc: added doc to instockcriteria test. Refs #19

--- filter/src/test/java/com/iluwatar/filter/InStockCriteriaTest.java
+++ filter/src/test/java/com/iluwatar/filter/InStockCriteriaTest.java
@@ -1,3 +1,28 @@
+/*
+ * This project is licensed under the MIT license. Module model-view-viewmodel is using ZK framework licensed under LGPL (see lgpl-3.0.txt).
+ *
+ * The MIT License
+ * Copyright © 2014-2022 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.iluwatar.filter;
 
 import com.iluwatar.filter.criteria.InStockCriteria;
@@ -9,8 +34,15 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Tests for in stock criteria.
+ */
 public class InStockCriteriaTest {
 
+    /**
+     * Tests that the filter works correctly.
+     * Asserts that of the three products added, the one that is not in stock is filtered out
+     */
     @Test
     void testSomeMeetsCriteria(){
         InStockCriteria criteria = new InStockCriteria();
@@ -28,6 +60,10 @@ public class InStockCriteriaTest {
         Assert.assertEquals(criteriaRes, productsInStock);
     }
 
+    /**
+     * Tests that the filter works correctly when no products meets the criteria.
+     * Asserts that all products are filtered out.
+     */
     @Test
     void testNoMeetsCriteria(){
         InStockCriteria criteria = new InStockCriteria();
@@ -43,6 +79,10 @@ public class InStockCriteriaTest {
         Assert.assertEquals(criteriaRes, productsInStock);
     }
 
+    /**
+     * Tests that the filter works correctly when all products meets the criteria.
+     * Asserts that no products are filtered out.
+     */
     @Test
     void testAllMeetsCriteria(){
         InStockCriteria criteria = new InStockCriteria();
